aae7ae04f58c2cb902db99c0d6824c658cb0eff6
Afshawn Lotfi <me@afshawn.org>
2020-08-10 05:12:43 +0000

    highlighter finished

--- packages/ui/src/components/animation/text-effects/highlighter.tsx
+++ packages/ui/src/components/animation/text-effects/highlighter.tsx
@@ -1,32 +1,35 @@
-import { makeStyles } from "@material-ui/core";
-import React from "react";
-import { classNames } from "../../../utils";
+import { makeStyles } from "@material-ui/core"
+import React from "react"
+import { classNames } from "../../../utils"
 
 const useStyles = makeStyles({
-
   onHover: {
-    backgroundPosition: "-100% 0"
+    backgroundPosition: "-100% 0",
   },
 
   title: {
-    background: "linear-gradient(to right, rgba(255, 255, 255, 0) 50%, #16a085 50%)",
+    background:
+      "linear-gradient(to right, rgba(255, 255, 255, 0) 50%, rgb(255,253,71) 50%)",
     backgroundSize: "200%",
     display: "inline",
-  }
-
+  },
 })
 
 export interface HighlighterProps {
   className: string
+  text: string
+  style?: React.CSSProperties
 }
 
-export const Highlighter: React.FC<HighlighterProps> = ({ className }) => {
+export const Highlighter: React.FC<HighlighterProps> = ({
+  className,
+  text,
+  style,
+}) => {
   const classes = useStyles()
   return (
-    <h1 className={classNames(classes.title, className)}>
-      This text will be highlighted when hovered
+    <h1 style={style} className={classNames(classes.title, className)}>
+      {text}
     </h1>
-
-
   )
 }
--- packages/ui/src/scene/animated-scene.tsx
+++ packages/ui/src/scene/animated-scene.tsx
@@ -6,66 +6,56 @@ import { highlight } from "../effects/highlight"
 import "./scene.css"
 
 export const AnimatedScene = () => {
-    const [isPlaying, changeIsPlaying] = useState(false)
-    const [sceneEl, changeSceneEl] = useState<Scene>()
-    const keyframes2 = {
-        ".textEx": {
-            // 0.2: wipeIn({ duration: 1, direction: "reverse" }),
+  const [isPlaying, changeIsPlaying] = useState(false)
+  const [sceneEl, changeSceneEl] = useState<Scene>()
+  const keyframes2 = {
+    ".textEx": {
+      // 0.2: wipeIn({ duration: 1, direction: "reverse" }),
 
-            // 0.2: wipeIn({ duration: 0.5, property: "bottom" }),
-            ...(highlight(0.2, 0.4))
+      // 0.2: wipeIn({ duration: 0.5, property: "bottom" }),
+      ...highlight(0.2, 0.4),
 
-            // 0.2: wipeIn({ duration: 1, direction: "reverse" })
+      // 0.2: wipeIn({ duration: 1, direction: "reverse" })
+    },
+  }
 
+  let inputEl: HTMLInputElement | null
 
+  return (
+    <AnimatedPlayer
+      isPlaying={isPlaying}
+      sceneEl={sceneEl}
+      setInputEl={(el) => {
+        inputEl = el
+      }}
+    >
+      <Scene
+        ref={(e) => {
+          changeSceneEl(e ?? undefined)
+          // @ts-ignore
+          window.scene = e?.getItem()
+        }}
+        keyframes={keyframes2}
+        easing={"ease-in-out"}
+        fillMode={"forwards"}
+        onAnimate={(e) => {
+          if (inputEl) {
+            inputEl.value = `${(100 * e.time) / e.currentTarget.getDuration()}`
+          }
+        }}
+        onPlay={() => {
+          changeIsPlaying(true)
+        }}
+        onPaused={() => {
+          changeIsPlaying(false)
+        }}
+      >
+        <Highlighter className={"textEx"} text={"HELLO WORLD"} />
 
-        }
-
-    }
-
-
-    let inputEl: HTMLInputElement | null
-
-    return (
-        <AnimatedPlayer isPlaying={isPlaying} sceneEl={sceneEl} setInputEl={(el) => {
-            inputEl = el
-        }}>
-            <Scene
-                ref={(e) => {
-                    changeSceneEl(e ?? undefined)
-                    // @ts-ignore
-                    window.scene = e?.getItem()
-                }}
-                keyframes={keyframes2}
-                easing={"ease-in-out"}
-                fillMode={"forwards"}
-                onAnimate={(e) => {
-                    if (inputEl) {
-                        inputEl.value = `${
-                            (100 * e.time) / e.currentTarget.getDuration()
-                            }`
-                    }
-                }}
-                onPlay={() => {
-                    changeIsPlaying(true)
-                }}
-                onPaused={() => {
-                    changeIsPlaying(false)
-                }}
-            >
-
-
-                <Highlighter className={"textEx"} />
-
-                {/* <AnimationElement top="90%" left="10%" className={"textEx"}>
+        {/* <AnimationElement top="90%" left="10%" className={"textEx"}>
                     <BlockTextEffect title="Afshawn" />
                 </AnimationElement> */}
-
-
-            </Scene>
-
-        </AnimatedPlayer>
-
-
-    )
+      </Scene>
+    </AnimatedPlayer>
+  )
 }
